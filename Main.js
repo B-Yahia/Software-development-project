ab09269d83cdc9e749f29dae6ec791b88c93dc67
B-Yahia <60832087+B-Yahia@users.noreply.github.com>
2020-10-27 23:51:34 +0200

    product page created

---
+++ Main.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAUdwPIH-YVD5IQilq6nYQkRT3z-R1Ji68",
+    authDomain: "finalproject-ca2f6.firebaseapp.com",
+    databaseURL: "https://finalproject-ca2f6.firebaseio.com",
+    projectId: "finalproject-ca2f6",
+    storageBucket: "finalproject-ca2f6.appspot.com",
+    messagingSenderId: "862539598761",
+    appId: "1:862539598761:web:4f888f9b7072c6fe1f12b8",
+    measurementId: "G-9HPWL1R83Z"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    
+    // Get a reference to the database service
+    var database = firebase.database();
+    
